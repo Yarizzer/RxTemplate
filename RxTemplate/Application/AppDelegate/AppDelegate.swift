68b75a544b347aa826a1f2cb00d27a2d8405726f
Yarizzer@gmail.com <Yarizzer@gmail.com>
2023-02-11 07:35:21 +0300

    Router added

---
+++ RxTemplate/Application/AppDelegate/AppDelegate.swift
@@ -0,0 +1,23 @@
+//
+//  AppDelegate.swift
+//  RxTemplate
+//
+//  Created by Yaroslav Abaturov on 11.02.2023.
+//
+
+import UIKit
+import CoreData
+
+@main
+class AppDelegate: UIResponder, UIApplicationDelegate {
+    private var router: AppRoutable?
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        // Override point for customization after application launch.
+        return true
+    }
+
+    private func routeToInitialScene() {
+        
+    }
+}
---
+++ RxTemplate/Application/AppRouter/AppRouter.swift
@@ -0,0 +1,28 @@
+//
+//  AppRouter.swift
+//  RxTemplate
+//
+//  Created by Yaroslav Abaturov on 11.02.2023.
+//
+
+import UIKit
+
+protocol AppRoutable {
+    func routeToInitialScene()
+}
+
+class AppRouter {
+    init() {
+        let window = UIWindow(frame: UIScreen.main.bounds)
+        window.backgroundColor = .systemBackground
+        self.window = window
+    }
+    
+    private var window: UIWindow
+}
+
+extension AppRouter: AppRoutable {
+    func routeToInitialScene() {
+        
+    }
+}
